9d94ff55b36f2d4dbc34826b273984f2bd5ca007
Sloan Quinn <sloan.quinn@gmail.com>
2021-02-19 02:35:48 -0600

    feat: added coordinate fetch from Mapquest API & implemented console output

--- src/weather.ts
+++ src/weather.ts
@@ -5,34 +5,135 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
 // set up yargs
-const args = yargs(hideBin(process.argv))
+const argv = yargs(hideBin(process.argv))
     .usage('USAGE: ts-node weather.ts [city] --options')
     .command({
         command: '$0 [city]',
         desc: 'Retrieves current and forecasted weather data for the named city. For city names' +
             'with spaces, use dash; i.e. \'San-Antonio\'. If no city is provided, defaults to ' +
             'the value set in the LOCAL environment variable.',
-        handler: defaultHandler
+        handler: defaultHandler,
+        array: true
     })
     .help()
     .argv;
 
-function defaultHandler(args: any) {
-    getWeatherData(args, 'weather');
+function defaultHandler(argv: any) {
+    processCommand(argv);
 }
 
-// Get weather data from OpenWeatherMap API
+async function processCommand(argv: any) {
+    await getCoords(argv);
+    getWeatherData(argv);
+}
 
-function getWeatherData(args: any, command: string) {
-    const reqURL = 'https://api.openweathermap.org/data/2.5/' + command + '?q=' +
-        ((args.city) ? args.city : process.env.LOCAL) + '&appid=' + process.env.API_KEY +
-        '&units=imperial'
+// Get city coordinates from Mapquest Geocoding API
+async function getCoords(argv: any) {
+    // build request URL
+    const reqURL = buildURL('geo', argv);
 
+    // fetch coords
+    const settings = { method: 'Get' };
+    await nfetch(reqURL, settings)
+        .then((res: any) => res.json())
+        .then((json: any) => {
+            if(json) {
+                const coords = json.results[0].locations[0].latLng;
+                argv.lat = coords.lat;
+                argv.lng = coords.lng;
+            } else {
+                console.error('GEOCODING API ERROR: City \'%s\' not found!', argv.city)
+            }
+        })
+        .catch((err: any) => console.log('Error: ' + err));
+
+}
+
+// Get weather data from Open Weather Map API
+function getWeatherData(argv: any) {
+
+    const reqURL = buildURL('weather', argv);
     const settings = { method: "Get" };
 
     nfetch(reqURL, settings)
         .then((res: any) => res.json())
-        .then((json: JSON) => {
-        console.log(json);
-    })
+        .then((json: any) => {
+            if(json.cod !== '404') {
+                formatOutput(json, argv);
+            } else {
+                console.error('WEATHER API ERROR: 404: City \'%s\' not found!', argv.city);
+            }
+
+    });
+}
+
+function buildURL(type: string, argv: any) {
+    if(type === 'geo') {
+        let url: string;
+        if( process.env.GEOCODING_FORMAT &&
+            process.env.GEOCODING_KEY &&
+            process.env.GEOCODING_BASE_URL) {
+
+            url = process.env.GEOCODING_FORMAT;
+            url = url.replace('${BASE_URL}', process.env.GEOCODING_BASE_URL)
+                .replace('${GEOCODING_KEY}', process.env.GEOCODING_KEY);
+
+            const options = 'location=' + ((argv.city) ? argv.city : process.env.LOCAL);
+
+            url = url.replace('${OPTIONS}', options);
+
+            return url;
+
+        } else {
+            console.error('ERROR: Geocoding API environment variables not set. Please ' +
+                'check your .env file.')
+        }
+    }
+    else if (type === 'weather') {
+        let url: string;
+        if( process.env.WEATHER_FORMAT &&
+            process.env.WEATHER_BASE_URL &&
+            process.env.WEATHER_KEY) {
+
+            url = process.env.WEATHER_FORMAT;
+            url = url.replace('${BASE_URL}', process.env.WEATHER_BASE_URL)
+                .replace('${WEATHER_KEY}', process.env.WEATHER_KEY);
+
+            const options = 'lat=' + argv.lat +
+                '&lon=' + argv.lng +
+                '&units=' + (argv.units ? argv.units : process.env.DEFAULT_UNITS);
+
+            url = url.replace('${OPTIONS}', options);
+
+            return url;
+        } else {
+            console.error('ERROR: Weather API environment variables not set. Please ' +
+                'check your .env file.')
+        }
+    }
+}
+
+
+function formatOutput(data: any, argv: any) {
+    console.log('\nWeather CLI\n');
+    console.log('City: %s', (argv.city) ? argv.city : process.env.LOCAL);
+    console.log('\nCURRENT WEATHER')
+    console.log('Temperature: %s deg %s',
+        data.current.temp,
+        (argv.units ? argv.units : (process.env.DEFAULT_UNITS==='imperial' ? 'Fahrenheit' : 'Celsius')));
+    console.log('Current conditions: %s', data.current.weather[0].main);
+    console.log('\nTOMORROW\'S WEATHER');
+    console.log('Temp (Low/High): %s/%s deg %s',
+        data.daily[1].temp.min,
+        data.daily[1].temp.max,
+        (argv.units ? argv.units : (process.env.DEFAULT_UNITS==='imperial' ? 'Fahrenheit' : 'Celsius')));
+    console.log('Conditions: %s', data.daily[1].weather[0].main);
+    if (data.alerts) {
+        console.log('\nALERTS');
+        data.alerts.forEach( (alert: any) => {
+            console.log('Agency: %s', alert.sender_name);
+            console.log('Alert: %s', alert.event);
+            console.log('\n%s', alert.description);
+        })
+    }
 }
